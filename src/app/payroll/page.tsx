d26c137a330c15647bb5bf88fbf32d73c27c31b2
NET HYR <lachicageo10@gmail.com>
2025-07-10 06:54:49 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/app/payroll/page.tsx
+++ src/app/payroll/page.tsx
@@ -1,3 +1,4 @@
+
 'use client';
 
 import { useState } from 'react';
@@ -24,6 +25,7 @@ import {
   DropdownMenuItem,
   DropdownMenuLabel,
   DropdownMenuTrigger,
+  DropdownMenuSeparator,
 } from '@/components/ui/dropdown-menu';
 import {
   Dialog,
